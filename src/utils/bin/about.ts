3a00df5451011d401794e00b4a5caeb3159b8659
joshua-le <jdl8176@nyu.edu>
2024-09-11 19:47:02 -0500

    feat: update about me section

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -19,12 +19,11 @@ const about = async (): Promise<string> => {
  ABOUT ME
  ${config.name}
  <u><a href="https://github.com/joshle298/JoshuaLeResume/blob/main/JoshuaLeResume.pdf" target="_blank">Resume</a></u>        
-爵 I’m a third-year undergraduate at NYU studying Computer Science 
+爵 I’m a senior at NYU studying Computer Science 
 with a minor in Mathematics. I've held the title of Software 
-Engineering Intern at Wayfair, Apple, and SendBlue (a startup).
+Engineering Intern at Gusto, Apple, Wayfair, and SendBlue (startup).
 Outside of school and work, I enjoy cooking new recipes,
-playing ultimate frisbee, and I'm currently training for the
-Ventura Marathon!
+playing ultimate frisbee, and field volleyball
 -----------
  CONTACT 
  <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
